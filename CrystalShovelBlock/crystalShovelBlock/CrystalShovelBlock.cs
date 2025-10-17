639449432d90f31ff460eada66291489a6c7dd4a
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2025-10-17 17:40:23 -0500

    Add files via upload

---
+++ CrystalShovelBlock/crystalShovelBlock/CrystalShovelBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Game
+{
+	// Token: 0x02000002 RID: 2
+	public class CrystalShovelBlock : ShovelBlock
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		public CrystalShovelBlock() : base(47, 172)
+		{
+		}
+
+		// Token: 0x04000001 RID: 1
+		public const int Index = 322;
+	}
+}
