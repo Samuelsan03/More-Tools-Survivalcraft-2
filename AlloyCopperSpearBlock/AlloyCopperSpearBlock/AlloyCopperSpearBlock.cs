cbd2423d8017bdfc6b25636e8f436c97f0934f85
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2025-10-17 17:22:08 -0500

    Add files via upload

---
+++ AlloyCopperSpearBlock/AlloyCopperSpearBlock/AlloyCopperSpearBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Game
+{
+	// Token: 0x02000002 RID: 2
+	public class AlloyCopperSpearBlock : SpearBlock
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		public AlloyCopperSpearBlock() : base(47, 77)
+		{
+		}
+
+		// Token: 0x04000001 RID: 1
+		public const int Index = 313;
+	}
+}
