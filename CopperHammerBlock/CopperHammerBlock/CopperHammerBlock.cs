2017fc99b597721384bc4ad30521b78581a2a230
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2025-10-17 17:25:22 -0500

    Add files via upload

---
+++ CopperHammerBlock/CopperHammerBlock/CopperHammerBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Game
+{
+	// Token: 0x02000002 RID: 2
+	public class CopperHammerBlock : HammerBlock
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		public CopperHammerBlock() : base(47, 79)
+		{
+		}
+
+		// Token: 0x04000001 RID: 1
+		public const int Index = 315;
+	}
+}
---
+++ DiamondHammerBlock/DiamondHammerBlock/DiamondHammerBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Game
+{
+	// Token: 0x02000002 RID: 2
+	public class DiamondHammerBlock : HammerBlock
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		public DiamondHammerBlock() : base(47, 182)
+		{
+		}
+
+		// Token: 0x04000001 RID: 1
+		public const int Index = 316;
+	}
+}
