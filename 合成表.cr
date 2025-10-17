24ba2712079e30f15a0edb89bb064658a0a4fe55
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2025-10-17 17:10:20 -0500

    Add files via upload

---
+++ 合成表.cr
@@ -0,0 +1,105 @@
+<Mod Name="0">
+<Recipes>
+	<Recipe Result="SteelShovelBlock" ResultCount="1" RequiredHeatLevel="0" a="ironshovel" b="ironingot" Description="Craft steel tools from iron tools and iron ingots">
+	 " b "
+	 "bab"
+	</Recipe>
+	<Recipe Result="SteelPickaxeBlock" ResultCount="1" RequiredHeatLevel="0" a="ironpickaxe" b="ironingot" Description="Craft steel tools from iron tools and iron ingots">
+	 " b "
+	 "bab"
+	</Recipe>
+	<Recipe Result="SteelAxeBlock" ResultCount="1" RequiredHeatLevel="0" a="ironaxe" b="ironingot" Description="Craft steel tools from iron tools and iron ingots">
+	 " b "
+	 "bab"
+	</Recipe>
+	<Recipe Result="SteelRakeBlock" ResultCount="1" RequiredHeatLevel="0" a="ironrake" b="ironingot" Description="Craft steel tools from iron tools and iron ingots">
+	 " b "
+	 "bab"
+	</Recipe>
+	<Recipe Result="SteelSpearBlock" ResultCount="1" RequiredHeatLevel="0" a="ironspear" b="ironingot" Description="Craft steel tools from iron tools and iron ingots">
+	 " b "
+	 "bab"
+	</Recipe>
+	<Recipe Result="SteelMacheteBlock" ResultCount="1" RequiredHeatLevel="0" a="ironmachete" b="ironingot" Description="Craft steel tools from iron tools and iron ingots">
+	 " b "
+	 "bab"
+	</Recipe>
+	<Recipe Result="SteelHammerBlock" ResultCount="1" RequiredHeatLevel="0" a="ironhammer" b="ironingot" Description="Craft steel tools from iron tools and iron ingots">
+	 " b "
+	 "bab"
+	</Recipe>
+	<Recipe Result="AlloyCopperShovelBlock" ResultCount="1" RequiredHeatLevel="0" a="coppershovel" b="copperingot" Description="Craft alloy copper tools from copper tools and copper ingots">
+	 " b "
+	 "bab"
+	</Recipe>
+	<Recipe Result="AlloyCopperPickaxeBlock" ResultCount="1" RequiredHeatLevel="0" a="copperpickaxe" b="copperingot" Description="Craft alloy copper tools from copper tools and copper ingots">
+	 " b "
+	 "bab"
+	</Recipe>
+	<Recipe Result="AlloyCopperAxeBlock" ResultCount="1" RequiredHeatLevel="0" a="copperaxe" b="copperingot" Description="Craft alloy copper tools from copper tools and copper ingots">
+	 " b "
+	 "bab"
+	</Recipe>
+	<Recipe Result="AlloyCopperRakeBlock" ResultCount="1" RequiredHeatLevel="0" a="copperrake" b="copperingot" Description="Craft alloy copper tools from copper tools and copper ingots">
+	 " b "
+	 "bab"
+	</Recipe>
+	<Recipe Result="AlloyCopperSpearBlock" ResultCount="1" RequiredHeatLevel="0" a="copperspear" b="copperingot" Description="Craft alloy copper tools from copper tools and copper ingots">
+	 " b "
+	 "bab"
+	</Recipe>
+	<Recipe Result="AlloyCopperMacheteBlock" ResultCount="1" RequiredHeatLevel="0" a="coppermachete" b="copperingot" Description="Craft alloy copper tools from copper tools and copper ingots">
+	 " b "
+	 "bab"
+	</Recipe>
+	<Recipe Result="AlloyCopperHammerBlock" ResultCount="1" RequiredHeatLevel="0" a="copperhammer" b="copperingot" Description="Craft alloy copper tools from copper tools and copper ingots">
+	 " b "
+	 "bab"
+	</Recipe>
+	<Recipe Result="CopperHammerBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="copperingot" Description="Craft copper tools from sticks and copper ingots">
+	 "bbb"
+	 "bab"
+	 " a "
+	</Recipe>
+	<Recipe Result="DiamondHammerBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="diamond" Description="Craft diamond tools from sticks and diamonds">
+	 "bbb"
+	 "bab"
+	 " a "
+	</Recipe>
+	<Recipe Result="CrystalShovelBlock" ResultCount="1" RequiredHeatLevel="0" a="diamondshovel" b="diamond" c="stick" Description="Craft crystal tools from diamond tools and diamonds">
+	 "bbb"
+	 "bab"
+	 " c "
+	</Recipe>
+	<Recipe Result="CrystalPickaxeBlock" ResultCount="1" RequiredHeatLevel="0" a="diamondpickaxe" b="diamond" c="stick" Description="Craft crystal tools from diamond tools and diamonds">
+	 "bbb"
+	 "bab"
+	 " c "
+	</Recipe>
+	<Recipe Result="CrystalAxeBlock" ResultCount="1" RequiredHeatLevel="0" a="diamondaxe" b="diamond" c="stick" Description="Craft crystal tools from diamond tools and diamonds">
+	 "bbb"
+	 "bab"
+	 " c "
+	</Recipe>
+	<Recipe Result="CrystalRakeBlock" ResultCount="1" RequiredHeatLevel="0" a="diamondrake" b="diamond" c="stick" Description="Craft crystal tools from diamond tools and diamonds">
+	 "bbb"
+	 "bab"
+	 " c "
+	</Recipe>
+	<Recipe Result="CrystalSpearBlock" ResultCount="1" RequiredHeatLevel="0" a="diamondspear" b="diamond" c="stick" Description="Craft crystal tools from diamond tools and diamonds">
+	 "bbb"
+	 "bab"
+	 " c "
+	</Recipe>
+	<Recipe Result="CrystalMacheteBlock" ResultCount="1" RequiredHeatLevel="0" a="diamondmachete" b="diamond" c="stick" Description="Craft crystal tools from diamond tools and diamonds">
+	 "bbb"
+	 "bab"
+	 " c "
+	</Recipe>
+	<Recipe Result="CrystalHammerBlock" ResultCount="1" RequiredHeatLevel="0" a="diamondhammer" b="diamond" c="stick" Description="Craft crystal tools from diamond tools and diamonds">
+	 "bbb"
+	 "bab"
+	 " c "
+	</Recipe>
+</Recipes>
+</Mod>
